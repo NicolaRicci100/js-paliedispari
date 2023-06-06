9421d90531d5ba38dfad3021e2ea1d9fe71b736a
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-06 14:50:03 +0200

    funzione per girare la stringa

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,2 +1,11 @@
 console.log('JS palindome OK');
 
+//* Funzione per girare una stringa
+function isPalindrome(userWord){
+    let reverseWord = '';
+    for (let i = userWord.length - 1; i >= 0; i--){
+        reverseWord += userWord[i];
+    }
+    return reverseWord;
+}
+console.log(isPalindrome('asd'));
