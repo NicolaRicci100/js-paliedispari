d781447d9987addea56720ee29086215c6855662
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-06 18:19:49 +0200

    soluzione temporanea

--- js/odd-even.js
+++ js/odd-even.js
@@ -21,12 +21,31 @@ const inputOdd = document.getElementById('odd');
 const inputEven = document.getElementById('even');
 
 inputEven.addEventListener('click', function(){
-    let choice = prompt ('scegli un numero da 1 a 5', '4');
+    let choice = prompt ('scegli un numero da 1 a 5', '4'); //faccio scegliere un numero all'utente
     console.log('il numero scelto è ' + choice);
-    const random = randomNumber();
-    let sum = parseInt(choice) + random;
+    const random = randomNumber(); //assegno una costante alla funzione randomNumber
     console.log('il numero casuale della CPU è ' + random);
+    let sum = parseInt(choice) + random; //sommo il numero generato casualemnte dalla cpu a quello dell'utente
     console.log('la somma è ' + sum);
-    isEven(sum);
-
+    isEven(sum); //dichiaro il risultato finale
+    if(sum % 2 == 0) {
+        console.log('Hai vinto');
+    }else{
+        console.log('Hai perso');
+    }
+})
+//! tantissima ripetizione di codice inutile - cercare altra soluzione
+inputOdd.addEventListener('click', function(){
+    let choice = prompt ('scegli un numero da 1 a 5', '2'); //faccio scegliere un numero all'utente
+    console.log('il numero scelto è ' + choice);
+    const random = randomNumber(); //assegno una costante alla funzione randomNumber
+    console.log('il numero casuale della CPU è ' + random);
+    let sum = parseInt(choice) + random; //sommo il numero generato casualemnte dalla cpu a quello dell'utente
+    console.log('la somma è ' + sum);
+    isEven(sum); //dichiaro il risultato finale
+    if(sum % 2 == 0) {
+        console.log('Hai perso');
+    }else{
+        console.log('Hai vinto');
+    }
 })
