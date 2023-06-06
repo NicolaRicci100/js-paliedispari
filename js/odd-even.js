8a28c360a2b6b0792a8af85ce5fbf55a3cd1e447
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-06 16:16:42 +0200

    funzione random cpu

--- js/odd-even.js
+++ js/odd-even.js
@@ -1,1 +1,10 @@
 console.log('JS odd-even OK');
+
+
+//* Funzione per randomizzare un numero da 1 a 5 per la CPU
+function randomNumber(min, max){
+    const number = Math.floor(Math.random() * 5) + 1;
+    return number;
+}
+const cpuNumber = randomNumber();
+console.log(cpuNumber);
