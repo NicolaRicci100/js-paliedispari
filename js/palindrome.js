d7aca1d2b86e35b01971a05870dadcd8faf3c0a8
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-06 15:30:19 +0200

    palindrome.js completo

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,11 +1,24 @@
 console.log('JS palindome OK');
+//* Richiamo le variabili dell'input e del bottone
+const inputWord = document.getElementById('input-word');
+const checkButton = document.getElementById('check-button');
 
-//* Funzione per girare una stringa
+//* Funzione per girare una stringa e dire se è o no palindroma
 function isPalindrome(userWord){
     let reverseWord = '';
     for (let i = userWord.length - 1; i >= 0; i--){
         reverseWord += userWord[i];
     }
+    if(reverseWord === userWord){
+        reverseWord = 'La parola è palindroma'
+    } else{
+        reverseWord = 'La parola non è palindroma'
+    }
     return reverseWord;
 }
-console.log(isPalindrome('asd'));
+//* Bottone di verifica
+checkButton.addEventListener('click', function(){
+    const answer = isPalindrome(inputWord.value);
+    console.log(answer);
+    document.getElementById('answer').innerHTML = answer;
+})
